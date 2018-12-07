a8de34ca66dbf8f27a7be04c05e032d3d11d2efc
Panos Kountanis <panosktn@gmail.com>
2018-12-07 18:56:54 +0200

    more elaborate app login simulation

--- app/app.py
+++ app/app.py
@@ -1,51 +1,78 @@
 """Subrequest example."""
 
-from flask import Flask, Response, request, make_response, render_template
-from urllib.parse import urlparse, parse_qs
+from flask import Flask, Response, request, make_response, render_template, redirect, url_for, session, abort
+from urllib.parse import urlparse
+import string
+from random import randint, choice
+
+
+RANDOM_CHAR_CHOICES = string.ascii_letters + string.digits
 
 
 app = Flask(__name__)
+app.secret_key = "l\xd3Z\x85\xbd ;R;\xbb'\xe3\xd5\x17\x9e\xfb"
+
+SITE_URL = 'https://demos.kountanis.com/app'
 
 
 AUTH_PARAM_NAME = '__auth'
+valid_tokens = set()
 
+
+@app.before_request
+def csrf_protect():
+    if request.method == "POST":
+        token = session.pop('_csrf_token', None)
+        if not token or token != request.form.get('_csrf_token'):
+            abort(403)
+
+
+def generate_csrf_token():
+    if '_csrf_token' not in session:
+        session['_csrf_token'] = generate_token()
+    return session['_csrf_token']
+
+
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
+
+def generate_token():
+    return "".join(choice(RANDOM_CHAR_CHOICES) for x in range(8))
 
 @app.route('/app')
 def index():
     """Index."""
-    return """
-    <!doctype html>
-    <html>
-    <head>
-        <title>Nginx Subrequests Example</title>
-    </head>
-    <body>
-        <div class="content">
-            <p>Try me!!</p>
-            <video src="http://localhost:8080/media/test_vid_hd.mp4?__auth=1" type="video/mp4" controls="controls" width="300"
-       height="200">Video not supported</video>
+    logged_in = request.cookies.get(AUTH_PARAM_NAME, None) is not None
+    res = make_response(render_template('index.html', logged_in=logged_in))
+    return res
 
-            <video src="http://localhost:8080/media/test_vid_hd.mp4" type="video/mp4" controls="controls" width="300"
-       height="200">Video not supported</video>
-        </div>
-    </body>
-    </html>
-    """
+
+@app.route('/login', methods=('POST',))
+def login():
+    token = request.cookies.get(AUTH_PARAM_NAME, None)
+    if token is None:
+        token = generate_token()
+        if token not in valid_tokens:
+            valid_tokens.add(token)
+    res = make_response(redirect('https://demos.kountanis.com/app'))
+    res.set_cookie(AUTH_PARAM_NAME, token)
+    return res
+
+@app.route('/logout', methods=('POST',))
+def logout():
+    res = make_response(redirect('https://demos.kountanis.com/app'))
+    res.set_cookie(AUTH_PARAM_NAME, '', expires=0)
+    return res
 
 
 @app.route('/auth')
 def auth():
-    """Authorize some times."""
+    """Authorize using cookie."""
     original_url = request.headers.get('x-original-uri', None)
 
     auth_cookie = request.cookies.get(AUTH_PARAM_NAME, None)
     if auth_cookie is None:
-        auth_cookie = request.args.get(AUTH_PARAM_NAME, None)
-    if auth_cookie is None and original_url is not None:
-        o = urlparse(original_url)
-        qs = parse_qs(o.query)
-        auth_cookie = qs.get(AUTH_PARAM_NAME, None)
-    if auth_cookie is None:
+        return Response('Unauthorized', 401)
+    if auth_cookie not in valid_tokens:
         return Response('Unauthorized', 401)
 
     return Response('OK', 200)
